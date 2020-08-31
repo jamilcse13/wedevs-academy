3634a8def1892e9a82b7d090e1ff60bde2b91d73
jamil <jamil.cse13@gmail.com>
2020-08-31 11:41:40 +0600

    Admin Menu Pages and Forms - CRUD Part 1

--- includes/Admin.php
+++ includes/Admin.php
@@ -6,6 +6,14 @@ class Admin
 {
     function __construct()
     {
+        $this->dispatch_actions();
+        
         new Admin\Menu();
+    }
+
+    public function dispatch_actions()
+    {
+        $addressbook = new Admin\Addressbook();
+        add_action( 'admin_init', [ $addressbook, 'form_handler'] );
     }
 }
--- includes/Admin/Menu.php
+++ includes/Admin/Menu.php
@@ -7,18 +7,48 @@ namespace WeDevs\Academy\Admin;
  */
 class Menu
 {
+    /**
+     * initialize the class
+     */
     function __construct() 
     {
         add_action( 'admin_menu', [ $this, 'admin_menu' ] );
     }
 
+    /**
+     * register admin menu
+     */
     public function admin_menu()
     {
-        add_menu_page( __( 'WeDevs Academy', 'wedevs-academy' ), __( 'Academy', 'wedevs-academy' ), 'manage_options', 'wedevs-academy', [ $this, 'plugin_page' ], 'dashicons-welcome-learn-more' );
+        $parent_slug = 'wedevs-academy';
+        $capability = 'manage_options';
+        add_menu_page( __( 'WeDevs Academy', 'wedevs-academy' ), __( 'Academy', 'wedevs-academy' ), $capability, $parent_slug, [ $this, 'addressbook_page' ], 'dashicons-welcome-learn-more' );
+        add_submenu_page( $parent_slug, __( 'Address Book', 'wedevs-academy' ), __( 'Address Book', 'wedevs-academy' ), $capability, $parent_slug, [ $this, 'addressbook_page' ] );
+        add_submenu_page( $parent_slug, __( 'Settings', 'wedevs-academy' ), __( 'Settings', 'wedevs-academy' ), $capability, 'wedevs-academy-settings', [ $this, 'settings_page' ] );
     }
 
-    public function plugin_page()
+    /**
+     * render the plugin page
+     */
+    // public function plugin_page()
+    // {
+    //     echo "Hello World";
+    // }
+
+    /**
+     * render the addressbook page
+     */
+    public function addressbook_page()
     {
-        echo "Hello World";
+        $addressbook = new Addressbook();
+        $addressbook->plugin_page();
+    }
+
+    /**
+     * render the settings page
+     */
+    public function settings_page()
+    {
+        echo "Hello Settings";
     }
 }
